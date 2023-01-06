3264a770ec4bceead0a5e443888b50c1cfabe4bf
rajatwgc <raj@wgc.net.in>
2023-01-06 13:50:17 +0530

    first

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import React, { useState } from "react";
+function App() {
+  return (
+ <>
+ <nav className="navbar navbar-expand-lg bg-light">
+  <div className="container-fluid">
+   <h1>Hello World</h1>
+
+    </div>
+          
+</nav>
+ </>
+  );
+}
+
+export default App;
